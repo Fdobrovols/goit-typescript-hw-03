f21865df87852455c1a4a90171751b50a9c9be4a
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-12-07 01:02:03 +0200

    fix task 4

--- src/4.ts
+++ src/4.ts
@@ -1,10 +1,63 @@
+
+class Key
+{
+    private signature: number = Math.random()
+    getSignature(): number
+    {
+        return this.signature;
+    }
+}
+
+class Person
+{
+    constructor(private key: Key)
+    {
+
+    }
+
+    getKey()
+    {
+        return this.key;
+    }
+}    
+
+abstract class House
+{
+    protected door: boolean = false;
+    protected tenats: Person[] = [];
+
+    constructor(protected key: Key) {}
+    
+    abstract openDoor(key: Key): void;
+    
+    comeIn(person: Person): void
+    {
+        if(this.door)
+        {
+            this.tenats.push(person);
+        }    
+    }
+
+}
+
+class MyHouse extends House
+{
+    openDoor(key: Key): void
+    {
+        if (key.getSignature() === this.key.getSignature())
+        {
+            this.door = true;
+        }    
+    }
+}    
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
 
 
